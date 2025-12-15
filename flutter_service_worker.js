6271acdbcb0d29b0316fb76dc7ef258c0995a7a5
Alessio Ripreso <ale.ripreso@gmail.com>
2025-12-15 01:19:16 +0100

    Gdad

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -10,7 +10,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "3bb5e52791612845e45639ed5044b5cb
 "assets/assets/icons/profile.svg": "82ddcb50039c6355dcf45c8a078c4427",
 "assets/assets/icons/settings.svg": "2e1b26f632f0202cc40ca3c7b9625dec",
 "assets/FontManifest.json": "5e79a7154a9c0743680ad7fe9f7d50c5",
-"assets/fonts/MaterialIcons-Regular.otf": "c6378a4f4c3c264334e979318e8d4fdf",
+"assets/fonts/MaterialIcons-Regular.otf": "2e06abbda2e7ea798ba819940d84546c",
 "assets/NOTICES": "4b3aef3a9f140709d2aaa0843d580d44",
 "assets/packages/flex_color_picker/assets/opacity.png": "49c4f3bcb1b25364bb4c255edcaaf5b2",
 "assets/packages/flutter_math_fork/lib/katex_fonts/fonts/KaTeX_AMS-Regular.ttf": "657a5353a553777e270827bd1630e467",
@@ -49,16 +49,16 @@ const RESOURCES = {"assets/AssetManifest.bin": "3bb5e52791612845e45639ed5044b5cb
 "canvaskit/skwasm_heavy.wasm": "b0be7910760d205ea4e011458df6ee01",
 "favicon.png": "909d951630e1757bdc819e80d3700ce4",
 "flutter.js": "24bc71911b75b5f8135c949e27a2984e",
-"flutter_bootstrap.js": "d892ef0886025c02c1d308f39a4c2cfd",
+"flutter_bootstrap.js": "43a4fd63d48cb376cefad9917e947a44",
 "icons/Icon-192.png": "b0c7ffdf7df2ad565a606485632956f7",
 "icons/Icon-512.png": "614af21790804deed114c58b71e59a31",
 "icons/Icon-maskable-192.png": "b0c7ffdf7df2ad565a606485632956f7",
 "icons/Icon-maskable-512.png": "614af21790804deed114c58b71e59a31",
 "index.html": "f02fd0d2fea157a121b839771f14971d",
 "/": "f02fd0d2fea157a121b839771f14971d",
-"main.dart.js": "089a42492206d7250b21513d18d365cd",
+"main.dart.js": "de379d8973c76fa9fef6059b2a577509",
 "manifest.json": "2ed0cd649023886d9d4f2461ab1418b2",
-"version.json": "bf4fef0fcb477cac9eea38bc088bf22e"};
+"version.json": "610f344e9529ae21079738f793a3e7c6"};
 // The application shell files that are downloaded before a service worker can
 // start.
 const CORE = ["main.dart.js",
